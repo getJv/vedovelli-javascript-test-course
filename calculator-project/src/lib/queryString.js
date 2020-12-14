9c7fd6b8af559aedbc25f7660e5ac3d9a343a989
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-12-14 10:14:23 -0300

    updating the project

--- calculator-project/src/lib/queryString.js
+++
@@ -1,8 +0,0 @@
-module.exports.queryString = (obj) => {
-    const queryParams = Object.entries(obj).map(item => {
-        return `${item[0]}=${item[1]}`
-    })
-
-    return queryParams.join('&')
-
-}
