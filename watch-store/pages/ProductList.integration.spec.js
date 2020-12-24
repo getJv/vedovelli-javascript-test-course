c1c517110c773a383a5e96219e047506942eda95
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-12-24 11:55:17 -0300

    update: class 50

--- watch-store/pages/ProductList.integration.spec.js
+++ watch-store/pages/ProductList.integration.spec.js
@@ -1,12 +1,37 @@
 import { mount } from '@vue/test-utils';
-import ProductList from '.';
+import axios from 'axios';
+import Vue from 'vue';
+import { makeServer } from '@/miragejs/server';
 import Productcard from '@/components/Productcard';
 import Search from '@/components/Search';
+import ProductList from '.';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+}));
 
 describe('ProductList - Integration', () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+  });
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('Should mount the component', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.vm).toBeDefined();
+  });
+  it('Should call axios.get when mount the component', () => {
+    mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith('/api/products');
   });
   it('Should mount Search component as a child', () => {
     const wrapper = mount(ProductList);
@@ -16,9 +41,65 @@ describe('ProductList - Integration', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.findComponent(Productcard)).toBeDefined();
   });
-  it('Should mount Productcard component 10 times', () => {
-    const wrapper = mount(ProductList);
+  it('Should mount Productcard component 10 times', async () => {
+    // MirageJs create the objects
+    const products = server.createList('product', 10);
+    // Mock the axios results with the objects
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+    // Create the componets
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+    // Give time to vue rendereing the component
+    await Vue.nextTick();
+    // tests
     const cards = wrapper.findAllComponents(Productcard);
     expect(cards).toHaveLength(10);
+  });
+  it('Should display the error when promise reject', async () => {
+    axios.get.mockReturnValue(Promise.reject(new Error('')));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    expect(wrapper.text()).toContain('Problemas ao carregar lista');
+  });
+  fit('filter the product list when search is perform', async () => {
+    // Arrange
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+      server.create('product', {
+        title: 'Meu outro relógio estimado',
+      }),
+    ];
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await Vue.nextTick();
+
+    // Act
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+
+    // Assert
+    const cards = wrapper.findAllComponents(Productcard);
+    expect(wrapper.vm.searchTerm).toEqual('relógio');
+    expect(cards).toHaveLength(2);
   });
 });
--- watch-store/pages/index.vue
+++ watch-store/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="my-8">
-    <search />
+    <search @doSearch="setSearchTerm" />
     <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
@@ -8,7 +8,7 @@
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
         <product-card
-          v-for="product in products"
+          v-for="product in list"
           :key="product.id"
           :product="product"
         />
@@ -29,7 +29,18 @@ export default {
     return {
       products: [],
       errorMessage: '',
+      searchTerm: '',
     };
+  },
+  computed: {
+    list() {
+      if (this.searchTerm !== '') {
+        return this.products.filter(({ title }) => {
+          return title.indexOf(this.searchTerm) > -1;
+        });
+      }
+      return this.products;
+    },
   },
   created() {
     this.fetchData();
@@ -42,6 +53,9 @@ export default {
       } catch (error) {
         this.errorMessage = 'Problemas ao carregar lista';
       }
+    },
+    setSearchTerm({ term }) {
+      this.searchTerm = term;
     },
   },
 };
